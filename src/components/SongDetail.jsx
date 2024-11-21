df672123cfa27d0b59d79050127e3075ead7d17f
sumit9722 <shahsumit9722@gmail.com>
2024-11-21 18:06:01 +0530

    player controls

--- src/components/SongDetail.jsx
+++ src/components/SongDetail.jsx
@@ -3,7 +3,10 @@ import "./SongDetail.css"
 export default function SongDetail(){
     return (
         <div className="songDetail">
-            
+            <div className="songDetailPic">
+                <img src="/samplesongpic.png" alt="samplepics" />
+            </div>
+            <div className="songName">Senior Name</div>
         </div>
     )
 }
