35df83d719aa23499850df9bd6ce99c05a14456c
sumit9722 <shahsumit9722@gmail.com>
2024-11-21 19:31:03 +0530

    resized

--- src/components/SongDetail.jsx
+++ src/components/SongDetail.jsx
@@ -4,7 +4,7 @@ export default function SongDetail(){
     return (
         <div className="songDetail">
             <div className="songDetailPic">
-                <img src="/samplesongpic.png" alt="samplepics" />
+                <img src="/samplesongpic.png" alt="samplepics" className="songDetailImg"/>
             </div>
             <div className="songName">Senior Name</div>
         </div>
