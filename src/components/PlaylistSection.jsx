e66fef6114e78b5713c80ddea5eb2abde1ce0d34
sumit9722 <shahsumit9722@gmail.com>
2024-11-23 16:22:50 +0530

    carousel blur

--- src/components/PlaylistSection.jsx
+++ src/components/PlaylistSection.jsx
@@ -9,7 +9,7 @@ export default function PlaylistSection(props){
             <PlaylistHead></PlaylistHead>
             <Playlist clickFunc = {props.clickFunc} items = {props.items} curr ={props.curr}></Playlist>
         </div>
-        <img src="/blur.png" alt="blur" className="blur" />
+            <img src="/blur.png" alt="blur" className="blur" />
         </>
     )
 }
